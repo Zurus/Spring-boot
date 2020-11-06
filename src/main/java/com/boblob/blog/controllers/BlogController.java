30c0cfbf9894813aa09a774b99512229cd70f913
Divaev.AM <Zulkund@mail.ru>
2020-11-06 17:17:41 +0500

    Исправление часового пояса

--- src/main/java/com/boblob/blog/controllers/BlogController.java
+++ src/main/java/com/boblob/blog/controllers/BlogController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by ADivaev on 03.11.2020.
@@ -23,4 +25,20 @@ public class BlogController {
         model.addAttribute("posts", posts);
         return "blog-main";
     }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text,  Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+
 }
